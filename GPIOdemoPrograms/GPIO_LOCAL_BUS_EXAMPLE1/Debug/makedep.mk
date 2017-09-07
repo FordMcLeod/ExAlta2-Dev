a1d07b240f3e657b68f4276f7d5e0edfacfdf407
FordMcLeod <omcleod@ualberta.ca>
2017-09-07 15:40:53 -0600

    Organized GPIO repo, forgot to stage previously

---
+++ GPIOdemoPrograms/GPIO_LOCAL_BUS_EXAMPLE1/Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\avr32\boards\uc3c_ek\led.c
+
+src\ASF\avr32\drivers\flashc\flashc.c
+
+src\ASF\avr32\drivers\gpio\gpio.c
+
+src\ASF\avr32\drivers\intc\exception.S
+
+src\ASF\avr32\drivers\intc\intc.c
+
+src\ASF\avr32\drivers\pm\pm_uc3c.c
+
+src\ASF\avr32\drivers\pm\power_clocks_lib.c
+
+src\ASF\avr32\drivers\scif\scif_uc3c.c
+
+src\ASF\avr32\utils\startup\startup_uc3.S
+
+src\ASF\avr32\utils\startup\trampoline_uc3.S
+
+src\gpio_local_bus_example.c
+
