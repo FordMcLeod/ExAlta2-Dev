d5dfeeb0fb4f653caea319556292513a6e68cb92
Stefan Damkjar <sdamkjar@ualberta.ca>
2017-09-18 18:26:43 -0600

    added dut mem test code

--- triumf/gecko/EFM32DUT/src/main.c
+++ triumf/gecko/EFM32DUT/src/main.c
@@ -3,10 +3,27 @@
 #include "em_usart.h"
 #include "em_gpio.h"
 
+#include <stdio.h>
+
 #include "InitDevice.h"
+
+
+#define BUFFER_SIZE 255
+#define TEST_SIZE 1024
+
+typedef unsigned int word;
+
+volatile word testMemArray[TEST_SIZE] = {0};
+unsigned int ram_errors = 0;
+word previous_write = 0;
+word next_write = 0;
+
+char buffer[BUFFER_SIZE] = {0};
 
 void PRINT_Stringln(USART_TypeDef* uart, char* data);
 void Delay(uint16_t milliseconds);
+void RAM_test(void);
+uint16_t CRC_calc(uint8_t *start, uint8_t *end);
 
 
 int main(void)
@@ -16,19 +33,34 @@ int main(void)
 
   enter_DefaultMode_from_RESET();
 
-  uint8_t toggle = 0;
+  uint32_t flashSize = 0;
+
+  uint16_t crc     = 0;
+  uint16_t old_crc = 0;
+
+  /* Find the size of the flash. DEVINFO->MSIZE is the
+   * size in KB so left shift by 10. */
+  flashSize = ((DEVINFO->MSIZE & _DEVINFO_MSIZE_FLASH_MASK) >> _DEVINFO_MSIZE_FLASH_SHIFT)
+              << 10;
 
   /* Infinite loop */
   while (1) {
-  	GPIO_PortOutSetVal(LED_PORT, toggle<<LED_PIN, 1<<LED_PIN);
-    toggle = !toggle;
-
     Delay(1000);
-
-    PRINT_Stringln(USART0,"\tHello!");
-  	
+  	GPIO_PinOutSet(LED_PORT,LED_PIN);
+    PRINT_Stringln(USART0,"ram");
+    RAM_test();
+    PRINT_Stringln(USART0,"fla");
+    crc = CRC_calc((void *) 0, (void *) flashSize);
+    if (old_crc != crc) {
+      sprintf(buffer,"CRC:%x",crc);
+      PRINT_Stringln(USART0,buffer);
+      old_crc = crc;
+    }
+    PRINT_Stringln(USART0,"end");
+    GPIO_PinOutClear(LED_PORT,LED_PIN);
   }
 }
+
 
 void PRINT_Stringln(USART_TypeDef* uart, char* data)
 {
@@ -41,6 +73,7 @@ void PRINT_Stringln(USART_TypeDef* uart, char* data)
   }
   USART_Tx(uart,'\n');
 }
+
 
 /******************************************************************************
  * @brief Delay function
@@ -66,4 +99,48 @@ void Delay(uint16_t milliseconds)
   
   /* Stop TIMER0 */
   TIMER0->CMD = TIMER_CMD_STOP;
+}
+
+
+void RAM_test(void) {
+  word read = 0;
+  unsigned int j = 0;
+  previous_write = next_write;
+  next_write = next_write ^ (word)(~0);
+  for(j = 0; j<TEST_SIZE; j++) {
+    read = testMemArray[j];
+    if (read != previous_write) {
+      // delayed read
+      sprintf(buffer,"DR:%0x,%0x,%0x",j,read,previous_write);
+      PRINT_Stringln(USART0,buffer);
+      ram_errors++; 
+    }
+    testMemArray[j]= next_write;
+    read = testMemArray[j];
+    if (read != next_write) {
+      // instantaneous read
+      sprintf(buffer,"IR:%0x,%0x,%0x",j,read,previous_write);
+      PRINT_Stringln(USART0,buffer);
+      ram_errors++;
+    }
+  }
+  sprintf(buffer,"RE:%u",ram_errors);
+  PRINT_Stringln(USART0,buffer);
+}
+
+
+uint16_t CRC_calc(uint8_t *start, uint8_t *end)
+{
+  uint16_t crc = 0x0;
+  uint8_t  *data;
+
+  for (data = start; data < end; data++)
+  {
+    crc  = (crc >> 8) | (crc << 8);
+    crc ^= *data;
+    crc ^= (crc & 0xff) >> 4;
+    crc ^= crc << 12;
+    crc ^= (crc & 0xff) << 5;
+  }
+  return crc;
 }
